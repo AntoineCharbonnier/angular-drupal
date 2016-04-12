185d3b27131abd41a417e53328ca9a288996099b
Charbonnier Antoine <charbonnierantoine@iMac-de-Antoine.local>
2016-04-12 17:20:18 +0200

    etablish slider start with ng model

--- d8client/app/scripts/controllers/main.js
+++ d8client/app/scripts/controllers/main.js
@@ -7,24 +7,39 @@
  * # MainCtrl
  * Controller of the d8clientApp
  */
-// angular.module('d8clientApp')
-//   .controller('MainCtrl', function ($scope) {
-//     $scope.awesomeThings = [
-//       'HTML5 Boilerplate',
-//       'AngularJS',
-//       'Karma'
-//     ];
-//   });
+
+angular.module('d8clientApp')
+ .controller('MainCtrl', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
+  var self = this;
+  self.showMe = 0;
+  // get the url of the drupal server url. like localhost but with vhost.conf
+  $http.get('http://d8server.local.lumini.fr/articles/').then(function(response) {
+    console.log('MainCtrl GET response', response);
+    var data         = response.data;
+    $scope.datas     = data;
+    $scope.pageClass = 'page-home';
+  });
+  self.increase = function(){
+    console.log("click", self);
+    self.showMe++;
+    console.log(self.showMe);
+  }
+
+}])
 
 
 angular.module('d8clientApp')
- .controller('MainCtrl', ['$scope', '$routeParams', '$http', function ($scope, $routeParams, $http) {
-
-   // get the url of the drupal server url. like localhost but with vhost.conf
-   $http.get('http://d8server.local.lumini.fr/articles/').then(function(response) {
-    console.log('MainCtrl GET response', response);
-    var data     = response.data;
-    console.log(data);
-    $scope.datas = data;
-   });
- }]);
+  .directive('controlsIndex', function(){
+    return {
+      restrict: 'C',
+      controller: 'MainCtrl',
+      link: function(scope, element, attrs, ctrl){
+        // ctrl.increase = function(){
+        //   console.log("click", ctrl);
+        //   ctrl.showMe++;
+        //   console.log(ctrl.showMe);
+        // }
+        
+      }
+    }
+  });
--- d8client/app/scripts/controllers/node.js
+++ d8client/app/scripts/controllers/node.js
@@ -14,26 +14,29 @@ angular.module('d8clientApp')
    var nid = $routeParams.nid;
 
    // get the url of the drupal server url. like localhost but with vhost.conf
-   $http.get('http://d8server.local.lumini.fr/node/' + nid + '?_format=hal_json').then(function(response) {
+  // $http.get('http://d8server.local.lumini.fr/node/' + nid + '?_format=hal_json').then(function(response) {
+  $http.get('http://d8server.local.lumini.fr/articles/' + nid + '?_format=hal_json').then(function(response) {
+
     console.log('NodeCtrl GET response', response);
     var data     = response.data;
-    $scope.type  = data.type[0].target_id;
-    $scope.title = data.title[0].value;
-    $scope.body  = data.body[0].value;
+    console.log("data", data);
+    $scope.title = data[0].title;
+    $scope.body  = data[0].body;
+    $scope.img   = data[0].field_image;
 
-    var o = data._links;
-
-    var arr = Object.keys(o).map(function(k) { return o[k] });
-
-    for( var i = 0; i < arr.length; i ++ ){
-      if (arr[i][0] != "undefined" && arr[i][0] != null) {
-        data = Object.keys(arr[i][0]).map(function(k) { return arr[i][0][k] });
-        if ( data[0].indexOf("png") > -1 || data[0].indexOf("jpg") > -1 ) {
-          console.log(data[0]);
-          $scope.img = data[0];
-        }
-      }
-    }
+    // old way with the node request
+    // $scope.type  = data[0].type.target_id;
+    // var o = data._links;
+    // var arr = Object.keys(o).map(function(k) { return o[k] });
+    // for( var i = 0; i < arr.length; i ++ ){
+    //   if (arr[i][0] != "undefined" && arr[i][0] != null) {
+    //     data = Object.keys(arr[i][0]).map(function(k) { return arr[i][0][k] });
+    //     if ( data[0].indexOf("png") > -1 || data[0].indexOf("jpg") > -1 ) {
+    //       console.log(data[0]);
+    //       $scope.img = data[0];
+    //     }
+    //   }
+    // }
 
     console.log('NodeCtrl GET $scope', $scope);
    });
